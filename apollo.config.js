3a46c7f0f3776a8edd5dfc6667aae5e8983ca58c
James Leung <jleung128hk@gmail.com>
2019-08-08 09:05:35 -0400

    Ready for aws deploy

---
+++ apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  client: {
+    includes: ["client/src/graphql/*.js"],
+    service: "books-authors"
+  }
+};
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+import path from "path";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 
-import { typeDefs, resolvers } from "./schema";
+import { typeDefs, resolvers } from "./src/schema";
 
 const server = new ApolloServer({
   typeDefs,
@@ -15,9 +15,17 @@ const server = new ApolloServer({
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Homepage"
+  });
+});
+
 app.use(cors());
 server.applyMiddleware({ app });
 
-app.listen(4000, () => {
-  console.log(`listening on port 4000${server.graphqlPath}`);
-});
+const port = process.env.PORT || 4000;
+
+app.listen(port);
